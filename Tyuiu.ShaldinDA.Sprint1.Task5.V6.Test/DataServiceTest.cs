cf3dba79d9b6168763577ba8abbf6a2c48d5ec3f
Данил Шалдин <steampanker45@gmail.com>
2024-10-04 15:53:57 +0500

    Task5

---
+++ Tyuiu.ShaldinDA.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ShaldinDA.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.ShaldinDA.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void DayOfWeek()
+        {
+            DataService ds = new DataService();
+            int res = ds.Calculate(1);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.ShaldinDA.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.ShaldinDA.Sprint1.Task5.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #1 | Выполнил: Шалдин Данил Андреевич | ИИПБ-24-1";
+        //Длинна строки 75 символов
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Преобразование типов и класс Convert                                    *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Шалдин Данил Андреевич | ИИПБ-24-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*Написать программу, которая решает следующую задачу: Пусть k – целое от  *");
+        Console.WriteLine("* 1 до 365. Присвоить целой переменной n значение 1,2,...,7 в зависимости *");
+        Console.WriteLine("* от того, на какой день недели (понедельник, вторник,..., воскресенье) пр*");
+        Console.WriteLine("*риходится k-й день невисокосного года, в котором 1 января – понедельник. *");
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int k;
+        Console.WriteLine("Введите номер дня (от 1 до 365):");
+        _ = int.TryParse(Console.ReadLine(), out k) && k >= 1 && k <= 365;
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+       
+        int result = ds.Calculate(k);
+        Console.WriteLine($"День недели для {k}-го дня: {result}");
+        Console.ReadKey();
+    }
+}
